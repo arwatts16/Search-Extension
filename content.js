eb9d8512d0ce408a376942fdc00b7fed720ba028
arwatts16 <alyssa.watts@caci.com>
2018-05-22 11:42:22 -0600

    organize

--- content.js
+++
@@ -1,9 +0,0 @@
-//alert("You're on a new page!!");
-
-chrome.runtime.onMessage.addListener(gotMessage); // if it gets a message, execute got Message
-
-function gotMessage(message, sender, sendResponse){
-
-    console.log(message);
-    
-}
