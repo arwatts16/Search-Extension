4b8866ea6284b13eea7ab9714539dc2908e1627f
arwatts16 <alyssa.watts@caci.com>
2018-05-22 11:26:24 -0600

    tracks changes in all items of popup

--- popup.js
+++ popup.js
@@ -2,46 +2,40 @@
 // Waits for popup to be loaded before it begins executing code
 document.addEventListener("DOMContentLoaded", function(event) { 
 
+    // gets active checkbox element
+    var active = document.getElementById('Active');
+
+    // assign event handler1
+    active.onclick = function(e) {
+        // toggles highlighting option on current tab
+        // Doesn't save unless the save button is hit
+    }
+
     //get the element1
     var input1 = document.getElementById('userinput1');
 
-    // assign event handler1
+    // assign event handler2
     input1.onchange = function(e) {
-        e.target.setAttribute('style', 'background-color:#D6D6FF');
-
-        chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, {type:"text"}, function(response){
-                console.log(response);
-            });
-        });
+        // search for input1.value words when user hits enter.
+        // Doesn't save unless the save button is hit
     }
-
 
     //get the element2
     var input2 = document.getElementById('userinput2');
 
-    // assign event handler2
+    // assign event handler3
     input2.onchange = function(e) {
-        e.target.setAttribute('style', 'background-color:#D6D6FF');
+        // search for input2.value words when user hits enter.
+        // Doesn't save unless the save button is hit
+    }
+
+    //check if save button gets clicked
+    var save = document.getElementById('Save');
+
+    save.onclick = function(e) {
+        console.log('save that stuff');
+        // saves any changes above 
     }
 
 });
 
-
-
-
-/*
-userInput1.input(Step2); //carry userInput1 to Step2 
-
-    function Step2(){   //finds the current tab carries that too Step3
-        let params = { currentView: true }
-
-        chrome.tabs.query(params, Step3);
-        
-        function Step3(tab){    //sends current tab id and user input to the content script
-            let message = userInput1.value();
-
-            chrome.tabs.sendMessage(tab.id, message);
-        }
-    }
-*/
