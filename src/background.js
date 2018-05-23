0d05cd06604aa29c7a57c454878a2b688bac0345
arwatts16 <alyssa.watts@caci.com>
2018-05-23 12:07:14 -0600

    popup and options now hold the values that they previously saved to localStorage

--- src/background.js
+++ src/background.js
@@ -1,18 +1,6 @@
 alert("background running");
 
-chrome.browserAction.onClicked.addListener(buttonClicked); // will NOT happen if there is a popup associated with the button
-
-function buttonClicked(tab) {
-    alert("button clicked!!");  
-
-    let msg = {
-        txt: "is it working?"
-    }
-
-    /* 
-       Sends a message to the content script in the specified tab 
-       (integer tabId, any message, object options, function responseCallback)
-       Content script will then run the chrome.runtime.onMessage event
-    */
-    chrome.tabs.sendMessage(tab.id, msg);
-}
+// create and initialize vars in localStorage
+localStorage.setItem('active', false);
+localStorage.setItem('input1', '');
+localStorage.setItem('input2', '');
