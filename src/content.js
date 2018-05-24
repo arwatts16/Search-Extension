8c71eb775aa0dbdc0fa1e23e129844d4ffbeb211
Alyssa Watts <alyssa.watts@comcast.net>
2018-05-24 10:12:21 -0600

    input1 and 2 keywords now save into an array

--- src/content.js
+++ src/content.js
@@ -1,12 +1,37 @@
+// global variables saying what and when to highlight
 var active = false;
-var input1 = '';
-var input2 = '';
+var input1 = {keywords : []};
+var input2 = {keywords : []};
 
+// when options or popup saves, this updates the global values
 chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
     if(msg.message == 'update'){
-        active = msg.message; // will all be saved as strings
-        input1 = msg.input1;
-        input2 = msg.input2;
+        var j = 0;
+        active = msg.active;
+
+        // store input1 keywords into an array
+        for(var i = 0; j < msg.input1.length; i++){
+            for(; msg.input1[j] !== ' ' && j < msg.input1.length; j++){
+                if(input1.keywords[i] == undefined){
+                    input1.keywords[i] = msg.input1[j];
+                }
+                else input1.keywords[i] += msg.input1[j];
+            }
+            j++;
+        }
+        
+        // reset j and store input 2 keywords into an array
+        j = 0;
+        for(var i = 0; j < msg.input2.length; i++){
+            for(; msg.input2[j] !== ' ' && j < msg.input2.length; j++){
+                if(input2.keywords[i] == undefined){
+                    input2.keywords[i] = msg.input2[j];
+                }
+                else input2.keywords[i] += msg.input2[j];
+            }
+            j++;
+        }
+
     }
 
 });
