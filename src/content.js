33f3626d71907da1fe047863e59b532a7638b7b6
Alyssa Watts <alyssa.watts@comcast.net>
2018-05-24 09:12:25 -0600

    sends active and both inputs to content and saves them there into local variables

--- src/content.js
+++ src/content.js
@@ -1,5 +1,12 @@
+var active = false;
+var input1 = '';
+var input2 = '';
+
 chrome.runtime.onMessage.addListener( function(msg, sender, sendResponse) {
     if(msg.message == 'update'){
-        alert(msg.message);
+        active = msg.message; // will all be saved as strings
+        input1 = msg.input1;
+        input2 = msg.input2;
     }
+
 });
--- src/save_load.js
+++ src/save_load.js
@@ -1,4 +1,4 @@
-  
+ 
 // Waits for popup to be loaded before it begins executing code
 document.addEventListener('DOMContentLoaded', function(event) { 
     
@@ -21,7 +21,12 @@ document.addEventListener('DOMContentLoaded', function(event) {
 
         // alert the content script that a change has been made
         chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
-			chrome.tabs.sendMessage(tabs[0].id, {message : 'update'}, function(response) {
+			chrome.tabs.sendMessage(tabs[0].id, {
+                message: 'update',
+                active : localStorage.getItem('active'),
+                input1 : localStorage.getItem('input1'),
+                input2 : localStorage.getItem('input2')
+            }, function(response) {
                 //window.close();
             });
 		});
