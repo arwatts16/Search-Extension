b9e4b8dd4d0559ac2a4edb8d1e8a2478ba9d4f36
Alyssa Watts <alyssa.watts@comcast.net>
2018-05-29 10:22:53 -0600

    when active is unchecked, keywords are no longer highlighted

--- src/content.js
+++ src/content.js
@@ -5,65 +5,79 @@ var input2 = { keywords: [] };
 
 // when options or popup saves, this updates the global values
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-  if (msg.message == 'update') {
-    var j = 0;
+  if (msg.message == "update") {
     active = msg.active;
-    input1.keywords = msg.input1.split(/(\W)/ && ' ');
-    input2.keywords = msg.input2.split(/(\W)/ && ' ');
-    if (active === 'true') {
-      highlight(input1.keywords, input2.keywords);
+    input1.keywords = msg.input1.split(/(\W)/ && " ");
+    input2.keywords = msg.input2.split(/(\W)/ && " ");
+    if (active === "true") {
+      findWords(input1.keywords, input2.keywords);
     } else {
-      location.reload();
+      reload();
     }
   }
 });
 
 //Node iterator goes through and finds matches from input 1 and 2
-function highlight(i1, i2) {
-  if (input1.keywords != '' || input2.keywords != '') {
+function findWords(i1, i2) {
+
+  if (input1.keywords != "" || input2.keywords != "") {
     var nodeIterator = null;
-    nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+    nodeIterator = document.createNodeIterator(
+      document.body,
+      NodeFilter.SHOW_TEXT
+    );
     var textNode = null;
     var numNodes = 0;
-    var numMatches1 = 0;
-    var numMatches2 = 0;
 
     while ((textNode = nodeIterator.nextNode()) !== null) {
       var parent = textNode.parentNode;
       var data = textNode.data;
-      var sepWords = data.split(/(\W)/ && ' ');
-      var indArr = [];
-      var parent;
-      for (var i = 0; i < i1.length && i1 != ''; i++) {
+      var sepWords = data.split(/(\W)/ && " ");
+
+      for (var i = 0; i < i1.length && i1 != ""; i++) {
         if (sepWords.includes(i1[i])) {
-          textNode.data = sepWords.join(' ');
+          textNode.data = sepWords.join(" ");
           var ind = textNode.data.indexOf(i1[i]);
-          textNode.data = textNode.data.replace(i1[i], '');
+          textNode.data = textNode.data.replace(i1[i], "");
           var newNode = textNode.splitText(ind);
-          var span = document.createElement('span');
+          var span = document.createElement("span");
           span.appendChild(document.createTextNode(i1[i]));
-          span.style.backgroundColor = 'yellow';
+          span.style.backgroundColor = "yellow";
+          span.className = "highlighted";
           parent.insertBefore(span, newNode);
-          numMatches1++;
           textNode = nodeIterator.nextNode();
           textNode = nodeIterator.nextNode();
         }
       }
-      for (var i = 0; i < i2.length && i2 != ''; i++) {
+      for (var i = 0; i < i2.length && i2 != ""; i++) {
         if (sepWords.includes(i2[i])) {
-          textNode.data = sepWords.join(' ');
+          textNode.data = sepWords.join(" ");
           var ind = textNode.data.indexOf(i2[i]);
-          textNode.data = textNode.data.replace(i2[i], '');
+          textNode.data = textNode.data.replace(i2[i], "");
           var newNode = textNode.splitText(ind);
-          var span = document.createElement('span');
+          var span = document.createElement("span");
           span.appendChild(document.createTextNode(i2[i]));
-          span.style.backgroundColor = '#79f2ff';
+          span.style.backgroundColor = "#79f2ff";
+          span.className = "highlighted";
           parent.insertBefore(span, newNode);
-          numMatches1++;
           textNode = nodeIterator.nextNode();
           textNode = nodeIterator.nextNode();
         }
       }
     }
+  }
+}
+
+function reload() {
+  var matches = document.getElementsByClassName('highlighted');
+  var parent;
+
+  for(var i = 0; i < matches.length; i++){
+    parent = matches[i].parentNode
+    matches[i].parentNode.innerHTML = parent.innerHTML.replace(matches[0].outerHTML, matches[0].innerText); 
+  }
+
+  if(document.getElementsByClassName('highlighted') !== null){
+    reload();
   }
 }
