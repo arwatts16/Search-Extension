77adf7bd5c14f688aa8a56ba6183260d61860cae
Lily Sikes <lctaft@mymail.mines.edu>
2018-05-29 11:31:13 -0600

    Merge issues

--- src/content.js
+++ src/content.js
@@ -6,25 +6,27 @@ var input2 = { keywords: [] };
 // when options or popup saves, this updates the global values
 chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
   if (msg.message == 'update') {
+    var j = 0;
     active = msg.active;
     input1.keywords = msg.input1.split(/(\W)/);
     input2.keywords = msg.input2.split(/(\W)/);
     if (active === 'true') {
       findWords(input1.keywords, input2.keywords);
     } else {
-      reload();
+      location.reload();
     }
   }
 });
 
 //Node iterator goes through and finds matches from input 1 and 2
 function findWords(i1, i2) {
-  reload();
   if (input1.keywords != '' || input2.keywords != '') {
     var nodeIterator = null;
     nodeIterator = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
     var textNode = null;
     var numNodes = 0;
+    var numMatches1 = 0;
+    var numMatches2 = 0;
 
     while ((textNode = nodeIterator.nextNode()) !== null) {
       var parent = textNode.parentNode;
@@ -33,15 +35,18 @@ function findWords(i1, i2) {
       var indArr = [];
       var parent;
       for (var i = 0; i < sepWords.length; i++) {
-        if (i1.includes(sepWords[i]) && !i1.includes('')) {
+        if (i1.includes(sepWords[i])) {
           highlight('yellow');
+          numMatches1++;
         } else if (i2.includes(sepWords[i]) && !i2.includes('')) {
           highlight('#79f2ff');
+          numMatches2++;
         }
       }
       function highlight(color) {
-        var ind = textNode.data.search(' ' + sepWords[i] + ' ');
-        textNode.data = textNode.data.replace(' ' + sepWords[i] + ' ', '  ');
+        var re = RegExp('\\W+' + sepWords[i] + '+\\W');
+        var ind = textNode.data.search(re);
+        textNode.data = textNode.data.slice(0, ind + 1) + textNode.data.slice(ind + 1 + sepWords[i].length);
         var newNode = textNode.splitText(ind + 1);
         var span = document.createElement('span');
         span.appendChild(document.createTextNode(sepWords[i]));
@@ -52,21 +57,5 @@ function findWords(i1, i2) {
         textNode = nodeIterator.nextNode();
       }
     }
-  }
-}
-
-function reload() {
-  var matches = document.getElementsByClassName('highlighted');
-  var parent;
-
-  if (document.getElementsByClassName('highlighted').length !== 0) {
-    for (var i = 0; i < matches.length; i++) {
-      parent = matches[i].parentNode;
-      matches[i].parentNode.innerHTML = parent.innerHTML.replace(matches[0].outerHTML, matches[0].innerText);
-    }
-  }
-
-  if (document.getElementsByClassName('highlighted').length !== 0) {
-    reload();
   }
 }
