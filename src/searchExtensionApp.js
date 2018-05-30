c044efc683c59fae220561ea566d295cb112be94
Lily Sikes <lctaft@mymail.mines.edu>
2018-05-30 08:00:25 -0600

    Search Extension App class skeleton created

---
+++ src/searchExtensionApp.js
@@ -0,0 +1,16 @@
+// App Main
+
+//Constructor
+bits.SearchExtension.searchExtensionApp = function() {};
+
+//This array holds all of the search providers or data sources
+bits.SearchExtension.searchExtensionApp.searchProviders = [];
+
+//Finds all the search providers
+bits.SearchExtension.searchExtensionApp.registerSearchProvider = function(iSearchProvider) {};
+
+bits.SearchExtension.searchExtensionApp.initSearchProviders = function() {};
+
+bits.SearchExtension.searchExtensionApp.querySearchProviders = function() {};
+
+bits.SearchExtension.searchExtensionApp.applySearchResults = function(searchResult) {};
