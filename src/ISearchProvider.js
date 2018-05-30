3f5538a54c0212e156aa1510af2cac32c4e30d32
Alyssa Watts <alyssa.watts@comcast.net>
2018-05-30 08:30:56 -0600

    setup ISearchProvider skeleton

---
+++ src/ISearchProvider.js
@@ -0,0 +1,10 @@
+bits.searchExtension.searchProvider = function() {};
+
+bits.searchExtension.searchProvider.prototype.query = function(document) {};
+
+bits.searchExtension.searchProvider.prototype.init = function() {};
+
+bits.searchExtension.searchProvider.prototype.setActive = function(active) {};
+
+//returns a boolean
+bits.searchExtension.searchProvider.prototype.getActive = function() {};
