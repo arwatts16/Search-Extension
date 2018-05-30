f72da6a65656a8a26179e6e15370f99ff7fc1382
Alyssa Watts <alyssa.watts@comcast.net>
2018-05-30 07:52:35 -0600

    setup result model

---
+++ src/searchResult.js
@@ -0,0 +1,13 @@
+// constructor
+
+bits.searchExtension.Result = function() {};
+
+bits.searchExtension.Result.prototype.id = null;
+
+bits.searchExtension.Result.prototype.name = null;
+
+bits.searchExtension.Result.prototype.type = null;
+
+bits.searchExtension.Result.prototype.source = null;
+
+bits.searchExtension.Result.prototype.element = null;
