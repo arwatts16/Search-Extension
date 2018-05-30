70f57e18e3168d7de4232256d38b0db624d2eecd
Lily Sikes <lctaft@mymail.mines.edu>
2018-05-30 08:10:18 -0600

    Popup control class skeleton created.

---
+++ src/searchExtensionPopupCtrl.js
@@ -0,0 +1,10 @@
+// This controls what the popup looks like
+
+//Constructor
+bits.SearchExtension.searchExtensionPopupCtrl = function() {};
+
+bits.SearchExtension.searchExtensionPopupCtrl.init = function() {};
+
+bits.SearchExtension.searchExtensionPopupCtrl.onActiveChange = function() {};
+
+bits.SearchExtension.searchExtensionPopupCtrl.onSearchProviderActiveChange = function() {};
