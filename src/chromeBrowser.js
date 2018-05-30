2a6b578cf7990017a05e4119dbf26472a353bc51
Lily Sikes <lctaft@mymail.mines.edu>
2018-05-30 08:47:03 -0600

    Chrome Browser skeleton created.

---
+++ src/chromeBrowser.js
@@ -0,0 +1,8 @@
+//Chrome brower extends iBrowser class
+
+//Constructor
+bits.SearchExtension.chromeBrowser = function() {
+  iBrowser.call(this);
+};
+
+bits.SearchExtension.iBrowser.name = 'Chrome';
