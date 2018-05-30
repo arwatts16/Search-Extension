dbf4d2fd25c298fffe60ab1e06c2b230ea69bdd9
Lily Sikes <lctaft@mymail.mines.edu>
2018-05-30 08:18:44 -0600

    Dom Control class skeleton created.

---
+++ src/searchExtensionDomCtrl.js
@@ -0,0 +1,14 @@
+// This class controls the Dom and manipulates the content of the browser
+
+//Constructor
+bits.SearchExtension.searchExtensionDomCtrl = function() {};
+
+bits.SearchExtension.searchExtensionDomCtrl.document = null;
+
+bits.SearchExtension.searchExtensionDomCtrl.browser = null;
+
+bits.SearchExtension.searchExtensionDomCtrl.initMatchManager = function() {};
+
+bits.SearchExtension.searchExtensionDomCtrl.initBrowser = function() {};
+
+bits.SearchExtension.searchExtensionDomCtrl.applyMatches = function(searchRes) {};
