eae941c4249ba4a4e8e0a97e5c3923d863758a03
Lily Sikes <lctaft@mymail.mines.edu>
2018-06-01 12:46:53 -0600

    apply results function called

--- src/appMain.js
+++ src/appMain.js
@@ -6,20 +6,7 @@ bits.search = function() {};
 //Constructor
 bits.search.appMain = function() {
   bits.search.appMain.registerSearchProvider();
-  chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
-    if (msg.message == 'send array') {
-      // updates isActive
-      searchProviders[0].provider.setActive(msg.cx);
-      searchProviders[1].provider.setActive(msg.uom);
-      bits.search.appMain.querySearchProviders('/data/uomData.json', '/data/cxData.json');
-
-      // sends data over to the content script
-      console.log('recieved sent message from dom');
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { message: 'sent data', data: searchProviders }, function(response) {});
-      });
-    }
-  });
+  bits.search.appMain.applySearchResults();
 };
 
 //This array holds all of the search providers or data sources
@@ -50,12 +37,18 @@ bits.search.appMain.querySearchProviders = function(uomFile, cxFile) {
   searchProviders[0].data = searchProviders[0].provider.query(cxFile);
 };
 
-bits.search.appMain.applySearchResults = function(searchResult) {
+bits.search.appMain.applySearchResults = function() {
   chrome.runtime.onMessage.addListener(function(msg, sender, sendResponse) {
     if (msg.message == 'send array') {
+      // updates isActive
+      searchProviders[0].provider.setActive(msg.cx);
+      searchProviders[1].provider.setActive(msg.uom);
+      bits.search.appMain.querySearchProviders('/data/uomData.json', '/data/cxData.json');
+
+      // sends data over to the content script
       console.log('recieved sent message from dom');
       chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        chrome.tabs.sendMessage(tabs[0].id, { message: 'sent data', data: sendResult }, function(response) {});
+        chrome.tabs.sendMessage(tabs[0].id, { message: 'sent data', data: searchProviders }, function(response) {});
       });
     }
   });
