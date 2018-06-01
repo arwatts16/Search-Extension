8a7caebc8be113dc00684d67f82c81fd8cede33f
Alyssa Watts <alyssa.watts@comcast.net>
2018-06-01 07:57:45 -0600

    implement querySearchProviders

--- src/appMain.js
+++ src/appMain.js
@@ -13,12 +13,30 @@ searchProviders = [];
 
 //Finds all the search providers
 bits.search.appMain.registerSearchProvider = function() {
- var uomSearch = new bits.search.uomFile();
- var uomData = uomSearch.query("/data/uomData.json");
+  var uom = new bits.search.uomFile();
+
+  var cx = new bits.search.cxFile();
+
+  searchProviders[0] = {
+    provider: uom,
+    data: null
+  };
+  searchProviders[1] = {
+    provider: cx,
+    data: null
+  };
+
+  bits.search.appMain.querySearchProviders(
+    "/data/uomData.json",
+    "/data/cxData.json"
+  );
 };
 
-bits.search.appMain.initSearchProviders = function() {};
+//bits.search.appMain.initSearchProviders = function() {};
 
-bits.search.appMain.querySearchProviders = function() {};
+bits.search.appMain.querySearchProviders = function(uomFile, cxFile) {
+  searchProviders[0].data = searchProviders[0].provider.query(uomFile);
+  searchProviders[1].data = searchProviders[1].provider.query(cxFile);
+};
 
 bits.search.appMain.applySearchResults = function(searchResult) {};
