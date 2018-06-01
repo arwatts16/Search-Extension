a25aa976560714535fc5ca714cdfda1866b6b829
Lily Sikes <lctaft@mymail.mines.edu>
2018-06-01 11:23:22 -0600

    New color added for cx data and default is set to show uom data if names overlap in both.

--- src/cxFile.js
+++ src/cxFile.js
@@ -1,6 +1,6 @@
 //global variables for SearchProvider
 var match;
-var isActive = "true";
+var isActive = 'true';
 
 //SearchProvider constructor which calls the ISearchProvider constructor
 bits.search.cxFile = function() {
@@ -8,9 +8,7 @@ bits.search.cxFile = function() {
 };
 
 //a new SearchProvider object which inherits from ISearchProvider
-bits.search.cxFile.prototype = Object.create(
-  bits.search.ISearchProvider.prototype
-);
+bits.search.cxFile.prototype = Object.create(bits.search.ISearchProvider.prototype);
 
 //set the constructor to refer to SearchProvider
 bits.search.cxFile.prototype.constructor = bits.search.cxFile;
@@ -19,8 +17,8 @@ bits.search.cxFile.prototype.constructor = bits.search.cxFile;
 bits.search.cxFile.prototype.query = function(doc) {
   var result = [];
   var uom = new XMLHttpRequest();
-  if (isActive === "true") {
-    uom.open("GET", doc, false);
+  if (isActive === 'true') {
+    uom.open('GET', doc, false);
 
     uom.send(null);
     var data = JSON.parse(uom.responseText);
@@ -31,7 +29,7 @@ bits.search.cxFile.prototype.query = function(doc) {
       result[i].name = data[i].name;
       result[i].type = data[i].type;
       result[i].subtype = data[i].subtype;
-      result[i].source = "uomFile";
+      result[i].source = 'cxFile';
     }
   }
   return result;
