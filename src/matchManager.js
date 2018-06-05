49378a0281990a4b2b4a3c028c5cde97f322bf6d
Lily Sikes <lctaft@mymail.mines.edu>
2018-06-05 08:23:22 -0600

    popup window comes up

--- src/matchManager.js
+++ src/matchManager.js
@@ -6,7 +6,7 @@ bits.search.matchManager = function() {};
 bits.search.matchManager.prototype.matches = [];
 
 bits.search.matchManager.prototype.matchAny = function(str, seRes) {
-  var re = RegExp('\\b' + seRes.name + '\\b');
+  var re = RegExp('\\b' + seRes.name + '\\b', 'i');
   if (re.test(str)) {
     bits.search.matchManager.prototype.matches.push(seRes);
     return true;
