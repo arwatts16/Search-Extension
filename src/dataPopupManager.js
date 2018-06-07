0f440f7b41ac4bcc93c197a3c30bb8437d8fbaa3
Lily Sikes <lctaft@mymail.mines.edu>
2018-06-07 14:19:46 -0600

    Dom control is now cleaned up and new icon was created.

---
+++ src/dataPopupManager.js
@@ -0,0 +1,29 @@
+/*
+* Data Popup Manager takes the matches and attaches the data
+*/
+
+bits.search.dataPopupManager = function(matches, found) {
+  for (var i = 0; i < matches.length; i++) {
+    matches[i].onclick = function(e) {
+      var popup = open('', 'Popup', 'width=300,height=200');
+      popup.document.title = 'Data';
+      popup.document.getElementsByTagName('body')[0].innerHTML = '';
+      var name = popup.document.createElement('h3');
+      var nameText = popup.document.createTextNode('Name: ' + found[e.srcElement.id].name);
+      name.appendChild(nameText);
+      popup.document.body.appendChild(name);
+      var type = popup.document.createElement('h3');
+      var typeText = popup.document.createTextNode('Type: ' + found[e.srcElement.id].type);
+      type.appendChild(typeText);
+      popup.document.body.appendChild(type);
+      var subtype = popup.document.createElement('h3');
+      var subtypeText = popup.document.createTextNode('Subtype: ' + found[e.srcElement.id].subtype);
+      subtype.appendChild(subtypeText);
+      popup.document.body.appendChild(subtype);
+      var source = popup.document.createElement('h3');
+      var sourceText = popup.document.createTextNode('Source: ' + found[e.srcElement.id].source);
+      source.appendChild(sourceText);
+      popup.document.body.appendChild(source);
+    };
+  }
+};
