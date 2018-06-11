b647e94c59072249ea697d664a0838ea16beea0c
Lily T Sikes <lily.sikes@caci.com>
2018-06-11 14:55:17 -0600

    Initial commit

--- src/appMain.js
+++ src/appMain.js
@@ -1,10 +1,17 @@
-var bits = function() {};
-bits.search = function() {};
+goog.provide('SearchExtensionMain');
+
+goog.require('goog.array');
+goog.require('goog.events.EventTarget');
+goog.require('goog.events.EventType');
+goog.require('goog.functions');
+goog.require('goog.object');
+var bits = function () { };
+bits.search = function () { };
 
 /*
  * Constructor
  */
-bits.search.appMain = function() {
+bits.search.appMain = function () {
   bits.search.appMain.registerSearchProvider('nxFile', 'file');
   bits.search.appMain.registerSearchProvider('cxSearch', 'search');
   bits.search.appMain.registerSearchProvider('uomFile', 'file');
@@ -20,7 +27,7 @@ bits.search.appMain.searchProviders = [];
  * Called by the constructor 
  * Whichever provider is added the the array last will take precedence
  */
-bits.search.appMain.registerSearchProvider = function(name, type) {
+bits.search.appMain.registerSearchProvider = function (name, type) {
   var sProvider = new bits.search.sProvider();
   if (type === 'file') {
     sProvider.provider = new bits.search.File(name);
@@ -34,7 +41,7 @@ bits.search.appMain.registerSearchProvider = function(name, type) {
  * Called by "send array" message from bMsgCenter.js
  * Sets data in searchProviders array
  */
-bits.search.appMain.querySearchProviders = function(dataSources) {
+bits.search.appMain.querySearchProviders = function (dataSources) {
   for (var i = 0; i < this.searchProviders.length; i++) {
     this.searchProviders[i].data = bits.search.appMain.searchProviders[i].provider.query(
       dataSources[i],
@@ -47,7 +54,7 @@ bits.search.appMain.querySearchProviders = function(dataSources) {
  * Called by "send array" message from bMsgCenter.js
  * Sets isActive for each provider and sets active inside searchProviders array
  */
-bits.search.appMain.setActiveProviders = function(activeSources) {
+bits.search.appMain.setActiveProviders = function (activeSources) {
   for (var i = 0; i < this.searchProviders.length; i++) {
     this.searchProviders[i].provider.setActive(activeSources[i]);
     this.searchProviders[i].active = activeSources[i];
